1d5b560267038fe679113813a2357daae0694a4d
DeepikaGit23 <dpardeshi5323@gmailcom>
2022-09-15 01:15:11 +0530

    Add Day 0

---
+++ Day0.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Day0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+        sc.close();
+        System.out.println("Hello, World.\n" + inputString);
+    }
+}
